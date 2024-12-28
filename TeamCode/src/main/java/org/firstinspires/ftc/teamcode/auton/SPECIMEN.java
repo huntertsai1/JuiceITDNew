f7302b867df2c60e7eb72d8b9027990c7cca4bc4
awesomehtr1 <awesomehtr1@gmail.com>
2024-12-28 13:54:09 -0800

    new pathing testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/SPECIMEN.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/SPECIMEN.java
@@ -7,6 +7,7 @@ import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -30,46 +31,36 @@ public class SPECIMEN extends LinearOpMode {
         drive = new PinpointDrive(hardwareMap, startPose);
 
         double HPDeposit = -46;
-        double spikeBack = -13;
+        double spikeBack = -12;
 
-        Action preload = drive.actionBuilder(startPose)
+        TrajectoryActionBuilder preload = drive.actionBuilder(startPose)
                 .lineToY(-33)
-                .waitSeconds(1)
-                .build();
+                .waitSeconds(0.2);
 
-        Action allSpikes = drive.actionBuilder(drive.pose)
-                .splineToLinearHeading(new Pose2d(38, -33, Math.toRadians(-90)), Math.toRadians(0))
-                .setTangent(Math.toRadians(90))
-                .lineToY(spikeBack)
+        TrajectoryActionBuilder allSpikes = preload.endTrajectory().fresh()
+                .splineToLinearHeading(new Pose2d(30, -38, Math.toRadians(-90)), Math.toRadians(0))
+                .splineToLinearHeading(new Pose2d(40, -12, Math.toRadians(-90)), Math.toRadians(90))
                 .setTangent(Math.toRadians(0))
                 .lineToX(44)
                 .setTangent(Math.toRadians(90))
                 .lineToY(HPDeposit)
-                .setTangent(Math.toRadians(0))
-                .lineToX(49)
-                .setTangent(Math.toRadians(90))
                 .lineToY(spikeBack)
                 .setTangent(Math.toRadians(0))
                 .lineToX(54)
                 .setTangent(Math.toRadians(90))
                 .lineToY(HPDeposit)
-                .setTangent(Math.toRadians(0))
-                .lineToX(59)
-                .setTangent(Math.toRadians(90))
                 .lineToY(spikeBack)
                 .setTangent(Math.toRadians(0))
-                .lineToX(63.5)
+                .lineToX(64)
                 .setTangent(Math.toRadians(90))
-                .lineToY(HPDeposit)
-                .build();
-
-        Action cycle1 = drive.actionBuilder(drive.pose)
-                .splineToLinearHeading(new Pose2d(24, -44, Math.toRadians(-45)), Math.toRadians(180))
-                .waitSeconds(0.5)
+                .lineToY(HPDeposit);
+        
+        TrajectoryActionBuilder cycle1 = allSpikes.endTrajectory().fresh()
+                .splineToLinearHeading(new Pose2d(16, -44, Math.toRadians(-45)), Math.toRadians(180))
+                .waitSeconds(0.2)
                 .setTangent(Math.toRadians(180))
-                .splineTo(new Vector2d(4, -33), Math.toRadians(135))
-                .waitSeconds(0.5)
-                .build();
+                .splineToLinearHeading(new Pose2d(6, -33, Math.toRadians(-90)), Math.toRadians(90))
+                .waitSeconds(0.2);
 
         robot.initSubsystems();
 
@@ -79,9 +70,9 @@ public class SPECIMEN extends LinearOpMode {
         Actions.runBlocking(
                 new ParallelAction(
                         new SequentialAction(
-                            preload,
-                            allSpikes,
-                            cycle1
+                            preload.build(),
+                            allSpikes.build(),
+                            cycle1.build()
                         ),
                         new LoopAction(() -> {
 //                            robot.lift.update();
