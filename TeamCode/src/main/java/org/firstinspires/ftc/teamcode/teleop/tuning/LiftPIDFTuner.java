a59ed6efdd2b3be85abf0dab6d7f8813cd400032
awesomehtr1 <awesomehtr1@gmail.com>
2024-12-30 20:10:45 -0800

    fix PID tuner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/tuning/LiftPIDFTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/tuning/LiftPIDFTuner.java
@@ -20,12 +20,11 @@ import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 public class LiftPIDFTuner extends OpMode {
     private PIDController controller1;
 
-    public double p = 0.02, i = 0.00, d = 0.00045;
-    public double f = 0.125;
+    public static double p = 0.02, i = 0.00, d = 0.00045;
+    public static double f = 0.125;
     public double voltageCompensation;
 
     public static int target = 0;
-    private final double angleSlides = 60;
 
     private DcMotorEx slides1;
     private DcMotorEx slides2;
@@ -43,9 +42,7 @@ public class LiftPIDFTuner extends OpMode {
 
         slides1.setDirection(DcMotorSimple.Direction.REVERSE);
         slides1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        slides2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         slides1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-//        slides2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
     @Override
@@ -55,20 +52,20 @@ public class LiftPIDFTuner extends OpMode {
         telemetry.addData("pos ", slides1Pos);
         double pid1 = controller1.calculate(slides1Pos, target);
         double ff = f;
+
         voltageCompensation = 13.2 / voltageSensor.getVoltage();
-//        double ff = Math.cos(Math.toRadians((double) target / (700/180.0)))*f;
 
         double power1 = (pid1 + ff) * voltageCompensation;
-        telemetry.addData("pow ", power1);
-        telemetry.addData("voltage compensation ", voltageCompensation);
+        telemetry.addData("POWER ", power1);
+        telemetry.addData("Voltage Compensation ", voltageCompensation);
 
         slides1.setPower(-power1);
         slides2.setPower(-power1);
 
-        telemetry.addData("pos1 ", slides1Pos);
-        telemetry.addData("target ", target);
-        telemetry.addData("motor 1 current", slides1.getCurrent(CurrentUnit.AMPS));
-        telemetry.addData("motor 2 current", slides2.getCurrent(CurrentUnit.AMPS));
+        telemetry.addData("POSITION ", slides1Pos);
+        telemetry.addData("TARGET ", target);
+        telemetry.addData("Motor 1 current", slides1.getCurrent(CurrentUnit.AMPS));
+        telemetry.addData("Motor 2 current", slides2.getCurrent(CurrentUnit.AMPS));
         telemetry.update();
     }
 }
