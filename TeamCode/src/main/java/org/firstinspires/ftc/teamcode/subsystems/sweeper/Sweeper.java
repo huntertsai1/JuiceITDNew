7176438a01f154db31a27aa4569551f1600253ed
Siddharth Ray <sidraystudios@gmail.com>
2025-02-09 16:42:02 -0800

    joos norcal sweep trust

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/sweeper/Sweeper.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.subsystems.sweeper;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.InstantAction;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.qualcomm.robotcore.hardware.configuration.BuiltInConfigurationTypeJsonAdapter;
+
+import org.firstinspires.ftc.teamcode.util.hardware.StepperServo;
+
+public class Sweeper {
+    StepperServo servo;
+
+    public float RETRACTED_POS = 0;
+    public float FULL_EXTENDED_POS = 240;
+    public double SWEEP_DELAY = 2;
+
+    public Sweeper(StepperServo s) {
+        servo = s;
+    }
+
+    public void setPosition(float pos) {
+        servo.setAngle(pos);
+    }
+
+    public Action sweep() {
+        return new SequentialAction(
+                new InstantAction(this::extend),
+                new SleepAction(SWEEP_DELAY),
+                new InstantAction(this::retract)
+        );
+    }
+
+    public void retract() {
+        setPosition(RETRACTED_POS);
+    }
+
+    public void extend() {
+        setPosition(FULL_EXTENDED_POS);
+    }
+}
