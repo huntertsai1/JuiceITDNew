347e875d4dfbaa02e63eb1a5c5376341330a74fa
Siddharth Ray <sidraystudios@gmail.com>
2025-06-17 14:16:19 -0700

    blinky

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/tuning/LEDTest.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.teleop.tuning;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.subsystems.lift.Lift;
+import org.firstinspires.ftc.teamcode.util.hardware.ContinuousServo;
+import org.firstinspires.ftc.teamcode.util.hardware.GoBildaLEDIndicator;
+import org.firstinspires.ftc.teamcode.util.hardware.Motor;
+import org.firstinspires.ftc.teamcode.util.hardware.StepperServo;
+
+@TeleOp(group = "competition")
+@Config
+public class LEDTest extends LinearOpMode {
+    public static double COLOR = 165;
+
+    GoBildaLEDIndicator blinky;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        blinky = new GoBildaLEDIndicator(1, "blinky", hardwareMap);
+
+
+        waitForStart();
+        if (isStopRequested()) return;
+        while (opModeIsActive() && !isStopRequested()) {
+            blinky.setColor(GoBildaLEDIndicator.Colors.JOOS_ORANGE);
+
+        }
+    }
+}
+
+
