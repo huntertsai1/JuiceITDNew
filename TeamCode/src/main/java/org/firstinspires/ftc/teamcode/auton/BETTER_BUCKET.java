392c00818e4bd8295a31719f358f767e9100a8b8
awesomehtr1 <awesomehtr1@gmail.com>
2025-06-22 14:51:48 -0700

    square 1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/BETTER_BUCKET.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/BETTER_BUCKET.java
@@ -10,9 +10,7 @@ import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.TranslationalVelConstraint;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
-import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 
@@ -21,16 +19,13 @@ import org.firstinspires.ftc.teamcode.commands.util.LoopAction;
 import org.firstinspires.ftc.teamcode.commands.WinchTimeAction;
 import org.firstinspires.ftc.teamcode.roadrunner.PinpointDrive;
 
-import java.util.List;
-
 
 @Autonomous(name = "BUCKET", group = "Autonomous")
-@Disabled
 public class BETTER_BUCKET extends LinearOpMode {
     Robot robot;
     PinpointDrive drive;
     public void runOpMode() {
-        Pose2d startPose = new Pose2d(-38, -62, Math.toRadians(90));
+        Pose2d startPose = new Pose2d(-38, -62, Math.toRadians(0));
         robot = new Robot (hardwareMap, true);
         drive = new PinpointDrive(hardwareMap, startPose);
 
@@ -39,169 +34,119 @@ public class BETTER_BUCKET extends LinearOpMode {
         double depositWait = 1.2;
         double intakeWait = 0.9;
 
-        double subSampleX = -6;
-        double subSampleY = -12;
-
         double veloLim = 60.0;
         double accelUpperLim = 60.0;
         double accelLowerLim = -40.0;
 
-        boolean oldDpadUp = false;
-        boolean oldDpadDown = false;
-        boolean oldDpadLeft = false;
-        boolean oldDpadRight = false;
-        boolean oldCircle = false;
-
         TrajectoryActionBuilder preload = drive.actionBuilder(startPose)
-        //preload
-            .setTangent(Math.toRadians(160))
-            .splineToLinearHeading(new Pose2d(depositX, depositY, Math.toRadians(60)), Math.toRadians(90))
-            .waitSeconds(depositWait);
-            //spike1 = preload.endTrajectory().fresh()
-            //spike1
-        TrajectoryActionBuilder intake1 = preload.endTrajectory().fresh()
-            .lineToY(-42, new TranslationalVelConstraint(20));
+                //preload
+                .setTangent(Math.toRadians(160))
+                .splineToLinearHeading(new Pose2d(depositX, depositY, Math.toRadians(45)), Math.toRadians(160))
+                .waitSeconds(0.8);
 
-        TrajectoryActionBuilder spike2 = intake1.endTrajectory().fresh()
-            //spike2
-            .setTangent(Math.toRadians(135))
-            .splineToLinearHeading(new Pose2d(-60, depositY, Math.toRadians(85)), Math.toRadians(135))
-            .setTangent(Math.toRadians(85));
-        TrajectoryActionBuilder intake2 = spike2.endTrajectory().fresh()
-            .lineToY(-42, new TranslationalVelConstraint(20))
-            .waitSeconds(intakeWait + depositWait);
+        TrajectoryActionBuilder spike1 = preload.endTrajectory().fresh()
+                //spike1
+                .setTangent(Math.toRadians(45))
+                .splineToLinearHeading(new Pose2d(-45, -48, Math.toRadians(106)), Math.toRadians(45))
+                .waitSeconds(intakeWait);
 
-        TrajectoryActionBuilder spike3 = intake2.endTrajectory().fresh()
+        TrajectoryActionBuilder deposit1 = spike1.endTrajectory().fresh()
+                //depo1
+                .setTangent(Math.toRadians(225))
+                .splineToLinearHeading(new Pose2d(depositX, depositY, Math.toRadians(30)), Math.toRadians(200))
+                .waitSeconds(depositWait);
+
+        TrajectoryActionBuilder spike2 = deposit1.endTrajectory().fresh()
+                //spike2
+                .setTangent(Math.toRadians(92))
+                .splineToLinearHeading(new Pose2d(-58, -48, Math.toRadians(112)), Math.toRadians(100))
+                .waitSeconds(intakeWait);
+
+        TrajectoryActionBuilder deposit2 = spike2.endTrajectory().fresh()
+                //depo2
+                .setTangent(Math.toRadians(272))
+                .splineToLinearHeading(new Pose2d(depositX, depositY, Math.toRadians(40)), Math.toRadians(272))
+                .waitSeconds(depositWait);
+
+        TrajectoryActionBuilder spike3 = deposit2.endTrajectory().fresh()
                 //spike3
-            .setTangent(Math.toRadians(150))
-            .splineToLinearHeading(new Pose2d(-62, -48, Math.toRadians(100)), Math.toRadians(150))
-            .setTangent(Math.toRadians(100));
-        TrajectoryActionBuilder intake3 = spike3.endTrajectory().fresh()
-            .lineToY(-40, new TranslationalVelConstraint(20))
-            .waitSeconds(intakeWait);
+                .setTangent(Math.toRadians(88))
+                .splineToLinearHeading(new Pose2d(-64.5, -45, Math.toRadians(119)), Math.toRadians(95))
+                .waitSeconds(intakeWait);
 
+        TrajectoryActionBuilder deposit3 = spike3.endTrajectory().fresh()
+                //depo3
+                .setTangent(Math.toRadians(268))
+                .splineToLinearHeading(new Pose2d(depositX, depositY, Math.toRadians(40)), Math.toRadians(268))
+                .waitSeconds(depositWait);
 
-        TrajectoryActionBuilder deposit3 = intake3.endTrajectory().fresh()
-//                //depo3
-            .setTangent(Math.toRadians(272))
-            .splineToLinearHeading(new Pose2d(-62, depositY, Math.toRadians(90)), Math.toRadians(272))
-            .waitSeconds(depositWait);
-
-
-
-//        TrajectoryActionBuilder intake4 = deposit3.endTrajectory().fresh()
-//                // sub intake
-//                .setTangent(Math.toRadians(90))
-//                .splineToLinearHeading(new Pose2d(subSampleX-20, subSampleY, Math.toRadians(0)), Math.toRadians(0))
-//
-//        TrajectoryActionBuilder deposit4 = intake4.endTrajectory().fresh()
-//                // depo 4
-//                .setTangent(Math.toRadians(90))
-//                .splineToLinearHeading(new Pose2d(depositX, depositY, Math.toRadians(40)), Math.toRadians(268))
-//
-        TrajectoryActionBuilder subDrive = deposit3.endTrajectory().fresh() // CHANGE TO DEPOSIT4 TO DRIVE BACK TO PARK AFTER SUB INTAKE + DEPO
-            //ascent zone park
-            .setTangent(Math.toRadians(90))
-            .splineToLinearHeading(new Pose2d(-48, -11, Math.toRadians(0)), Math.toRadians(0))
-            .lineToX(-20);
+        TrajectoryActionBuilder subDrive = deposit3.endTrajectory().fresh()
+                //ascent zone park
+                .setTangent(Math.toRadians(90))
+                .splineToLinearHeading(new Pose2d(-48, -11, Math.toRadians(0)), Math.toRadians(0))
+                .lineToX(-20);
 
         robot.initSubsystems();
-        List<LynxModule> allHubs = hardwareMap.getAll(LynxModule.class);
 
-        for (LynxModule module : allHubs) {
-            module.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
-        }
-        while (gamepad1.circle == false){
-            if (gamepad1.dpad_down && !oldDpadDown) {
-                subSampleY -= 1;
-            }
-            if (gamepad1.dpad_up && !oldDpadUp) {
-                subSampleY += 1;
-            }
-            if (gamepad1.dpad_left && !oldDpadLeft) {
-                subSampleX -= 1;
-            }
-            if (gamepad1.dpad_right && !oldDpadRight) {
-                subSampleX += 1;
-            }
-            oldDpadUp = gamepad1.dpad_up;
-            oldDpadDown = gamepad1.dpad_down;
-            oldDpadLeft = gamepad1.dpad_left;
-            oldDpadRight = gamepad1.dpad_right;
-        }
-
-        gamepad1.circle = false;
         waitForStart();
 
         if (isStopRequested()) return;
         Actions.runBlocking(
-            new ParallelAction(
-                new InstantAction(() -> robot.claw.setPower(0)),
-                new SequentialAction(
-                    new ParallelAction(
-                            preload.build(),
-                            robot.autoHighBasketAction()
-                    ),
-                    robot.outtakeSample(true),
+                new ParallelAction(
+                        new InstantAction(() -> robot.claw.setPower(0)),
+                        new SequentialAction(
+                                new ParallelAction(
+                                        preload.build(),
+                                        robot.autoHighBasketAction()
+                                ),
+                                robot.outtakeSample(true),
 
-                    new ParallelAction(
-                        robot.autoBucketIntake(true),
-                        intake1.build()
-                    ),
+                                spike1.build(),
 
-                    new SleepAction(1),
+                                robot.autoBucketIntake(true),
+                                new SleepAction(1),
 
-                    robot.autoHighBasketAction(),
-                    robot.outtakeSample(true),
-                    spike2.build(),
+                                new InstantAction(() -> robot.claw.setPower(0)),
+                                new ParallelAction(
+                                        robot.autoHighBasketAction(),
+                                        deposit1.build()
+                                ),
+                                robot.outtakeSample(true),
 
-                    new ParallelAction(
-                            robot.autoBucketIntake(true),
-                            intake2.build()
-                    ),
-                    new SleepAction(1),
+                                spike2.build(),
 
-                    robot.autoHighBasketAction(),
-                    robot.outtakeSample(true),
+                                robot.autoBucketIntake(true),
+                                new SleepAction(1),
 
-                    spike3.build(),
-                    new ParallelAction(
-                        robot.autoBucketIntake(true),
-                        intake3.build()
-                    ),
+                                new InstantAction(() -> robot.claw.setPower(0)),
+                                new ParallelAction(
+                                        robot.autoHighBasketAction(),
+                                        deposit2.build()
+                                ),
+                                robot.outtakeSample(true),
 
-                    new ParallelAction(
-                            robot.autoHighBasketAction(),
-                            deposit3.build()
-                    ),
-                    robot.outtakeSample(true),
+                                spike3.build(),
 
-//                    intake4.build(),
-//
-//                    robot.autoBucketIntake(true),
-//                    new SleepAction(1),
-//
-//                    new InstantAction(() -> robot.claw.setPower(0)),
-//                    new ParallelAction(
-//                            robot.autoHighBasketAction(),
-//                            deposit4.build()
-//                    ),
-//                    robot.outtakeSample(true),
+                                robot.autoBucketIntakeTHIRD(true),
+                                new SleepAction(1),
 
-                    new SleepAction(1),
-                    subDrive.build(),
-                    robot.sweeper.sweep(),
-                    new SleepAction(1),
-                    robot.sweeper.sweep()
-                ),
-                new LoopAction(() -> {
-                    for (LynxModule module : allHubs) {
-                        module.clearBulkCache();
-                    }
-                    robot.lift.update();
-                }, this::isStopRequested)
-                , new WinchTimeAction(robot.climbWinch, 2.2, -1, telemetry)
-            )
+                                new InstantAction(() -> robot.claw.setPower(0)),
+                                new ParallelAction(
+                                        robot.autoHighBasketAction(),
+                                        deposit3.build()
+                                ),
+                                robot.outtakeSample(true),
+                                new SleepAction(1),
+                                subDrive.build(),
+                                robot.sweeper.sweep(),
+                                new SleepAction(1),
+                                robot.sweeper.sweep()
+                        ),
+                        new LoopAction(() -> {
+                            robot.lift.update();
+                        }, this::isStopRequested)
+                        , new WinchTimeAction(robot.climbWinch, 2.2, -1, telemetry)
+                )
         );
     }
 }
